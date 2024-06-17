fdb51f1879d8d1fd676f54bbc9eeaea5366cc5b2
KafetzisThomas <tomkafetzis06@gmail.com>
2024-06-17 15:52:37 +0300

    Add salt to hashed message

--- main.py
+++ main.py
@@ -4,23 +4,43 @@
 # Project Title: Message-Hasher (https://github.com/KafetzisThomas/Message-Hasher)
 # Author / Project Owner: KafetzisThomas (https://github.com/KafetzisThomas)
 
+import os
 import sys
 import bcrypt
 import hashlib
 
 
-def hash_message(hash_algo, message):
-    if hash_algo == "bcrypt": hashed_message = bcrypt.hashpw(message.encode(), bcrypt.gensalt(10))
-    if hash_algo == "md5": hashed_message = hashlib.md5(message.encode()).hexdigest()
-    if hash_algo == "sha1": hashed_message = hashlib.sha1(message.encode()).hexdigest()
-    if hash_algo == "sha224": hashed_message = hashlib.sha224(message.encode()).hexdigest()
-    if hash_algo == "sha256": hashed_message = hashlib.sha256(message.encode()).hexdigest()
-    if hash_algo == "sha384": hashed_message = hashlib.sha384(message.encode()).hexdigest()
-    if hash_algo == "sha512": hashed_message = hashlib.sha512(message.encode()).hexdigest()
-    return message, hash_algo, hashed_message
-    
+def generate_salt(length=16):
+    """Generate random salt"""
+    return os.urandom(length)
 
-if __name__ == '__main__':
+
+def hash_message(hash_algo, message):
+    """Hash message using specified algorithm with a salt"""
+    salt = generate_salt()
+    salted_message = message.encode("utf-8") + salt
+
+    if hash_algo == "bcrypt":
+        hashed_message = bcrypt.hashpw(message.encode(), bcrypt.gensalt(10))
+    elif hash_algo == "md5":
+        hashed_message = hashlib.md5(salted_message).hexdigest()
+    elif hash_algo == "sha1":
+        hashed_message = hashlib.sha1(salted_message).hexdigest()
+    elif hash_algo == "sha224":
+        hashed_message = hashlib.sha224(salted_message).hexdigest()
+    elif hash_algo == "sha256":
+        hashed_message = hashlib.sha256(salted_message).hexdigest()
+    elif hash_algo == "sha384":
+        hashed_message = hashlib.sha384(salted_message).hexdigest()
+    elif hash_algo == "sha512":
+        hashed_message = hashlib.sha512(salted_message).hexdigest()
+    else:
+        raise ValueError(f"Unsupported hash algorithm: {hash_algo}")
+
+    return salt, hashed_message
+
+
+if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("[!] Usage: python3 main.py <hash_algo> <message>")
         sys.exit()
@@ -28,5 +48,9 @@ if __name__ == '__main__':
     hash_algo_input = sys.argv[1]
     message_input = sys.argv[2]
 
-    message, hash_algo, hashed_message = hash_message(hash_algo_input, message_input)
-    print(f"Message: {message}\nHash algo: {hash_algo}\nHashed message: {hashed_message}")
+    salt, hashed_message = hash_message(hash_algo_input, message_input)
+
+    print(f"Message: {message_input}")
+    print(f"Hash Algorithm: {hash_algo_input}")
+    print(f"Salt (hex): {salt.hex()}")
+    print(f"Hashed Message: {hashed_message}")
