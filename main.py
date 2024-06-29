e72f6d270e61a5db3d6773cfd923e2c55027c4a6
KafetzisThomas <tomkafetzis06@gmail.com>
2024-06-29 14:02:40 +0300

    Clean ups

--- main.py
+++ main.py
@@ -26,33 +26,31 @@ def generate_salt(length=16):
 
 
 def hash_message(hash_algo, message):
-    """Hash message using specified algorithm with a salt"""
-    salt = generate_salt()
-    salted_message = message.encode("utf-8") + salt
-
+    """Hash message using specified algorithm with a salt."""
     if hash_algo == "bcrypt":
-        hashed_message = bcrypt.hashpw(message.encode(), bcrypt.gensalt(10))
-    elif hash_algo in SHA_HASH_ALGORITHMS:
+        salt = bcrypt.gensalt(10)
+        hashed_message = bcrypt.hashpw(message.encode(), salt)
+        return salt, hashed_message
+    else:
+        salt = generate_salt()
+        salted_message = message.encode("utf-8") + salt
+
         hash_func = getattr(hashlib, hash_algo)
         hashed_message = hash_func(message.encode()).hexdigest()
         hashed_message_salted = hash_func(salted_message).hexdigest()
-    else:
-        raise ValueError(f"Unsupported hash algorithm: {hash_algo}")
 
-    return salt, hashed_message, hashed_message_salted
+        return salt, hashed_message, hashed_message_salted
 
 
 def verify_message(hash_algo, stored_hash, stored_salt, message_to_check):
     """Verify the provided message against the stored hash with salt."""
-    salted_message = message_to_check.encode("utf-8") + stored_salt
-
     if hash_algo == "bcrypt":
         return bcrypt.checkpw(message_to_check.encode(), stored_hash)
-    elif hash_algo in SHA_HASH_ALGORITHMS:
+    else:
+        salted_message = message_to_check.encode("utf-8") + stored_salt
         hash_func = getattr(hashlib, hash_algo)
         hash_to_check = hash_func(salted_message).hexdigest()
-
-    return hash_to_check == stored_hash
+        return hash_to_check == stored_hash
 
 
 if __name__ == "__main__":
@@ -60,15 +58,22 @@ if __name__ == "__main__":
         print("[!] Usage: python3 main.py <hash_algo> <message>")
         sys.exit()
 
-    hash_algo_input = sys.argv[1]
+    hash_algo_input = sys.argv[1].lower()
     message_input = sys.argv[2]
+    hashed_message_salted = None
 
-    salt, hashed_message, hashed_message_salted = hash_message(
-        hash_algo_input, message_input
-    )
-    is_valid = verify_message(
-        hash_algo_input, hashed_message_salted, salt, message_input
-    )
+    if hash_algo_input == "bcrypt":
+        salt, hashed_message = hash_message(hash_algo_input, message_input)
+        is_valid = verify_message(hash_algo_input, hashed_message, None, message_input)
+    elif hash_algo_input in SHA_HASH_ALGORITHMS:
+        salt, hashed_message, hashed_message_salted = hash_message(
+            hash_algo_input, message_input
+        )
+        is_valid = verify_message(
+            hash_algo_input, hashed_message_salted, salt, message_input
+        )
+    else:
+        raise ValueError(f"Unsupported hash algorithm: {hash_algo_input}")
 
     print(f"Message: {message_input}")
     print(f"Hash Algorithm: {hash_algo_input}")
