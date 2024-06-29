33036f9a0603d131abe1be7264cb49e9d57f60b4
KafetzisThomas <tomkafetzis06@gmail.com>
2024-06-29 13:42:11 +0300

    Display hashed messages with & without salt

--- main.py
+++ main.py
@@ -34,11 +34,12 @@ def hash_message(hash_algo, message):
         hashed_message = bcrypt.hashpw(message.encode(), bcrypt.gensalt(10))
     elif hash_algo in SHA_HASH_ALGORITHMS:
         hash_func = getattr(hashlib, hash_algo)
-        hashed_message = hash_func(salted_message).hexdigest()
+        hashed_message = hash_func(message.encode()).hexdigest()
+        hashed_message_salted = hash_func(salted_message).hexdigest()
     else:
         raise ValueError(f"Unsupported hash algorithm: {hash_algo}")
 
-    return salt, hashed_message
+    return salt, hashed_message, hashed_message_salted
 
 
 def verify_message(hash_algo, stored_hash, stored_salt, message_to_check):
@@ -62,11 +63,16 @@ if __name__ == "__main__":
     hash_algo_input = sys.argv[1]
     message_input = sys.argv[2]
 
-    salt, hashed_message = hash_message(hash_algo_input, message_input)
-    is_valid = verify_message(hash_algo_input, hashed_message, salt, message_input)
+    salt, hashed_message, hashed_message_salted = hash_message(
+        hash_algo_input, message_input
+    )
+    is_valid = verify_message(
+        hash_algo_input, hashed_message_salted, salt, message_input
+    )
 
     print(f"Message: {message_input}")
     print(f"Hash Algorithm: {hash_algo_input}")
     print(f"Salt (hex): {salt.hex()}")
     print(f"Hashed Message: {hashed_message}")
+    print(f"Hashed Message (+salt): {hashed_message_salted}")
     print(f"Message is valid: {is_valid}")
